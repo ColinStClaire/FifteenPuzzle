3267ef499bd2307beb48fdf7c1fab56dcf553ced
Colin M St. Claire <colin.stclaire@wsu.edu>
2018-02-12 18:26:28 -0800

    Initial Commit

---
+++ FifteenPuzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FifteenPuzzle
+//
+//  Created by Colin St. Claire on 2/12/18.
+//  Copyright © 2018 Colin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
